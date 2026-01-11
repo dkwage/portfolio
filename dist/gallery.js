500b3135e112da9c23460ddc1f648baec0529cac
David Kang <dkwage@inha.edu>
2026-01-11 20:51:17 +0900

    del portraits

--- dist/gallery.js
+++ dist/gallery.js
@@ -42,18 +42,6 @@ const images = [
   {
     href: './dist/assets/photo/14.webp',
   },
-  {
-    href: './dist/assets/photo/15.webp',
-  },
-  {
-    href: './dist/assets/photo/16.webp',
-  },
-  {
-    href: './dist/assets/photo/17.webp',
-  },
-  {
-    href: './dist/assets/photo/18.webp',
-  },
 ];
 
 // 갤러리 컨테이너 가져오기
